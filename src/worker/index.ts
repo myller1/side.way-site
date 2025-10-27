dd10e13707bfd0566b731113394aa391e88ca4f9
myller1 <henriquexazevedo@gmail.com>
2025-10-27 17:05:25 -0300

    Atualização Site 1

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,6 +1,18 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/cloudflare-workers";
 import { Env } from "./types";
 
 const app = new Hono<{ Bindings: Env }>();
+
+// Servir arquivos estáticos com manifest vazio (será preenchido pelo Wrangler)
+app.use("/*", serveStatic({ 
+  manifest: {} 
+}));
+
+// Fallback para SPA - todas as rotas não encontradas retornam o index.html
+app.get("*", serveStatic({ 
+  path: "./index.html",
+  manifest: {}
+}));
 
 export default app;
